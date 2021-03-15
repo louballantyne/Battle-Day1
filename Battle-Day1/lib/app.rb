5074814245225befc860db0e278b3a66db20763d
IchchadhariNaag <ralphgbartley@gmail.com>
2021-03-15 17:17:22 +0000

    need to add some CSS
    Co-authored-by: Lou <lou@birdline.co.uk>

--- Battle-Day1/lib/app.rb
+++ Battle-Day1/lib/app.rb
@@ -1,5 +1,24 @@
 require 'sinatra'
+require 'shotgun'
+
+configure do
+  enable :sessions
+  set :session_secret, "secret"
+end
 
 get '/' do
   "Hello World"
+end
+
+get '/secret' do
+  "one more time please"
+end
+
+get '/parrot-pizza-parlor' do
+  "Parrots are NOT on the pizza"
+end
+
+get '/cat' do
+  "div { border: medium dashed red; }
+  <div><img src='https://www.vets4pets.com/siteassets/species/cat/kitten/tiny-kitten-in-sunlight.jpg'></div>"
 end
